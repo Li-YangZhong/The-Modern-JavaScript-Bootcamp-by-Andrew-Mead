4e4bbe43edc86555e058eb5fdf9bf33202714181
Eric <liyangzhong@hotmail.com>
2020-01-02 07:18:19 +0800

    update

---
+++ arrays/notes.js
@@ -0,0 +1,19 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+// console.log(notes.pop())//remove last item
+// notes.push('My new note')//add to the end
+
+// console.log(notes.shift())//remove first item
+// notes.unshift('My first note')//add to the beginning
+
+// notes.splice(1, 1, 'This is the new second item')//start at index 1 and remove item(s) or add new item(s)
+
+notes[2] = 'This is now the new note 3'
+
+notes.forEach(function (item, index) {
+    console.log(index)
+    console.log(item)
+})
+
+console.log(notes.length)
+console.log(notes)
