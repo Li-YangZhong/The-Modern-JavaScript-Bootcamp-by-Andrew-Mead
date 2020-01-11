f5607acecca1d9bfa935df78d0201f85969b4940
Eric <liyangzhong@hotmail.com>
2020-01-11 23:27:54 +0800

    update

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -16,13 +16,18 @@ const todos = [{
 }]
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    hideCompleted: false
 }
 
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function(todo) {
-        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompleteMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompleteMatch
     })
+
 
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.completed 
@@ -43,18 +48,23 @@ const renderTodos = function (todos, filters) {
 
 renderTodos(todos, filters)
 
-// Listen for new todo creation
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('Add a new todo...')
-})
-
-//Listen for todo text change
-document.querySelector('#new-todo-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
-})
-
 document.querySelector('#search-text').addEventListener('input', function(e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 
+})
+
+document.querySelector('#new-todo').addEventListener('submit', function(e) {
+    e.preventDefault()
+    todos.push({
+        text:e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
+
+document.querySelector('#hide-completed').addEventListener('change', function(e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
 })
