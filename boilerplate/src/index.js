bb103fcc0e558a98d796fb44140a043c23780df7
Eric <liyangzhong@hotmail.com>
2020-01-31 14:43:08 +0800

    object spread syntax

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,18 +1,50 @@
-const calculateAverage = (thing, ...numbers) => {
-  // return (numOne + numTwo) / 2
-  let sum = 0
-  numbers.forEach((num) => sum += num)
-  const average = sum / numbers.length
-  return `The average ${thing} is ${average}`
-
-}
-
-console.log(calculateAverage('age',0, 100, 88, 64))
-
 const printTeam = (teamName, coach, ...players) => {
   console.log(`Team: ${teamName}`)
   console.log(`Coach: ${coach}`)
   console.log(`Players: ${players.join(', ')}`)
 }
 
-printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+const team = {
+  name: 'Liberty',
+  coach: 'Case Penn',
+  players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+}
+
+printTeam(team.name, team.coach, ...team.players)
+
+let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
+cities = [...cities, 'Santiago']
+
+console.log(cities)
+
+let house = {
+  bedrooms: 2,
+  bathrooms: 1.5,
+  yearBuilt: 2017
+}
+
+let newHouse = {
+  basement: true,
+  bedrooms: 3,
+  ...house
+
+}
+console.log(house)
+console.log(newHouse)
+
+const person = {
+  name: 'Andrew',
+  age: 27
+}
+
+const location = {
+  city: 'Philadelphia',
+  country: 'USA'
+}
+
+const overview = {
+  ...person,
+  ...location
+}
+
+console.log(overview)
