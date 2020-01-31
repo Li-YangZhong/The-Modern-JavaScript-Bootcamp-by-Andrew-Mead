d9eb7328e056065aa8e69d087799a251388bc71a
Eric <liyangzhong@hotmail.com>
2020-01-31 15:27:05 +0800

    last update

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,50 +1,22 @@
-const printTeam = (teamName, coach, ...players) => {
-  console.log(`Team: ${teamName}`)
-  console.log(`Coach: ${coach}`)
-  console.log(`Players: ${players.join(', ')}`)
+const todo = {
+  id: 'sdflwefwesdfs',
+  text: 'Pay the bills',
+  completed: false
 }
 
-const team = {
-  name: 'Liberty',
-  coach: 'Case Penn',
-  players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
+const printTodo = ({ text, completed }) => {
+  console.log(`${text} : ${completed}`)
 }
+printTodo(todo)
 
-printTeam(team.name, team.coach, ...team.players)
+const { text:todoText, completed, details = 'No details provided', ...others} = todo
 
-let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
-cities = [...cities, 'Santiago']
+console.log(todoText)
+console.log(completed)
+console.log(details)
+console.log(others)
 
-console.log(cities)
-
-let house = {
-  bedrooms: 2,
-  bathrooms: 1.5,
-  yearBuilt: 2017
-}
-
-let newHouse = {
-  basement: true,
-  bedrooms: 3,
-  ...house
-
-}
-console.log(house)
-console.log(newHouse)
-
-const person = {
-  name: 'Andrew',
-  age: 27
-}
-
-const location = {
-  city: 'Philadelphia',
-  country: 'USA'
-}
-
-const overview = {
-  ...person,
-  ...location
-}
-
-console.log(overview)
+const age = [65, 0, 13]
+const [firstAge, ...otherAges] = age
+console.log(firstAge)
+console.log(otherAges)
